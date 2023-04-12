e70c9f4e161f40e5f276e21ae6e97cfb11b12231
Mergim <mergimberisha0@gmail.com>
2023-04-12 14:43:28 +0100

    checked if password passes the test

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,7 +1,7 @@
 require "./lib/password_checker.rb"
 
 RSpec.describe PasswordChecker do
-  context "testing if the password is long enough" do
+  context "testing if the password is long enough with errors" do
     it "fails" do
       password = PasswordChecker.new
       expect { password.check("small") }.to raise_error "Invalid password, must be 8+ characters."
@@ -9,6 +9,12 @@ RSpec.describe PasswordChecker do
     it "It fails because rabbit is shorter than 8 characters" do
       password = PasswordChecker.new
       expect { password.check("rabbit") }.to raise_error "Invalid password, must be 8+ characters."
+    end
+  end
+  context "testing if the password is long enough" do
+    it "passes" do
+      password = PasswordChecker.new
+      expect(password.check("password")).to eq true
     end
   end
 end
